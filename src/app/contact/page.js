9a142a5f013b3bcaaa580307c9e1e395dde5f8e3
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2025-07-17 03:23:55 +0400

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -130,7 +130,7 @@ export default function Contact() {
                             </div>
                             <div className={styles.infoItem}>
                                 <h3>PHONE</h3>
-                                <p>+995 599 96 21 42</p>
+                                <p>+995 595 59 43 42</p>
                             </div>
                             <div className={styles.infoItem}>
                                 <h3>EMAIL</h3>
